d29c03d348e55a9fc9c858ddd7b8ea67ab4783af
JSharp0127 <152332448+JSharp0127@users.noreply.github.com>
2023-12-03 19:56:54 -0500

    Add files via upload

---
+++ Character_Creator/Character_Creator/text.py
@@ -0,0 +1,47 @@
+
+
+
+def GLOSSARY():
+    glossary_str = ""
+    return glossary_str
+
+
+def HELP_TEXT():
+    help_str = ""
+    return help_str
+
+
+def SPLASH_TEXT():
+    splash_str = (
+        "Welcome to \"Easy 5e Character Creator\"\n" +
+        "This application is a solo project by JSharp0127\n" +
+        "The Purpose of this application is to make creating a player character for the \nTTRPG Dungeons and Dragons 5th Edition\n" +
+        "an easy and quick expirence for new and veterain players that want to quickly jump into playing with their friends.\n"  +
+        "This application aids in creating a level 1-3 character following the SRD-OGL creation rules.\n" +
+        "Meaning while the player can choose their race/class combination and roll their stats, there are parts of the\n" +
+        "creation that are set by default.\n" +
+        "Such things are:\n" +
+        "Skills\nSub-Race\nSub-Class\nSpells\n" +
+        "While the goal is to make this application more customizable in the future for the time being using these\n" +
+        "restricted choice selections makes it so the application can be released in a stable condition so it can be\n" +
+        "accessed asap for players to use.\n"
+        "\n"+
+        "There is an  indepth How to Create a Character explanation under the Help menu, but the majority\n" +
+        "of creation consists of player choices from the provided drop down menues, rolling and compilation\n" +
+        "buttons, and name entry field.\n" +
+        "Under the Help menu there is also a provided Glossary for explaining any abbriviated text.\n" +
+        "(i.e. DMG = Damage or STR = Strength)\n" +
+        "\n" +
+        "Update Log:\n" +
+        "11/31/23 - Stats are added and automatically update\n" +
+        "(Players have the ability to roll their main 6 stats and HP. AC, modifiers, and skills automatically update\n" +
+        "using the results of the main stats.)\n" +
+        "12/1/23 - Races are added\n" +
+        "(Players now can choose their race and the race feature appear in the features block)\n" +
+        "12/3/23 - The start of classes has been added\n" +
+        "(The first 4 classes (Barbarian, Bard, Cleric, and Druid) have been added and their class features\n" +
+        "appear in the features block)\n" +
+        "12/4/23  - Saving and Loading\n" +
+        "(Character sheets can now be saved and loaded using the characters name for the file name)\n")
+    return splash_str
+
